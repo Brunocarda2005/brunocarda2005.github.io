20c38a0f6dc9f0083df14a8603df9e538225ac4e
bruno <97346838+Brunocarda2005@users.noreply.github.com>
2022-04-02 19:47:26 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+const form = document.querySelector(".form");
+const boton = form.querySelector("#adjuntar");
+const input = form.querySelector('#input-arch');
+
+boton.addEventListener('click' , sudar()); 
+
+function sudar(){
+  
+  input.click()
+  
+}
