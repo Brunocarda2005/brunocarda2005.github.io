c003c670cc7cdbc8603d1f278a6e902ee302a49a
bruno <97346838+Brunocarda2005@users.noreply.github.com>
2022-04-13 05:45:26 -0300

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function curriculum (){
     let confirmo = confirm( "Do you want this resume in English?");
     if (confirmo == false) {
     
-      cv.setAttribute("href" , "important/cv español.docx");
+      cv.setAttribute("href" , "cv español.docx");
       cv.removeEventListener("click" , curriculum)
       
     }else {
